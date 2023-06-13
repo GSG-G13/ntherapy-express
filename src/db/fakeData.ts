ca647d990220c9e82eca61f3e387ce8f8b56dd73
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-06-13 12:19:35 +0300

    feat: add more fake data to appintments

--- src/db/fakeData.ts
+++ src/db/fakeData.ts
@@ -148,7 +148,7 @@ const Admins = [
 const Therapists = [
   {
     id: 1,
-    cvLink: 's3://bucket-name/cv_12132132454516.pdf',
+    cvLink: 'https://drive.google.com/file/d/1jthZD5IlhwMn5IBUoQ-IPj3vnsuVsXf9/view?usp=sharing',
     profileImg: 'https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg',
     major: 'Mental Health Counselor',
     bio: "A counselor with a master's degree and several years of supervised clinical work experience. Trained to diagnose and provide individual and group counseling. ",
@@ -157,7 +157,7 @@ const Therapists = [
   },
   {
     id: 2,
-    cvLink: 's3://bucket-name/cv_11111111111111.pdf',
+    cvLink: 'https://drive.google.com/file/d/1jthZD5IlhwMn5IBUoQ-IPj3vnsuVsXf9/view?usp=sharing',
     profileImg: 'https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg',
     major: 'Licensed Professional Counselor',
     bio: " A counselor with a master's degree in psychology, counseling or a related field. Trained to diagnose and provide individual and group counseling ",
@@ -166,7 +166,7 @@ const Therapists = [
   },
   {
     id: 3,
-    cvLink: 's3://bucket-name/cv_222222222222.pdf',
+    cvLink: 'https://drive.google.com/file/d/1jthZD5IlhwMn5IBUoQ-IPj3vnsuVsXf9/view?usp=sharing',
     profileImg: 'https://www.felixhospital.com/sites/default/files/2022-11/dr-dk-gupta.jpg',
     major: 'Nurse Psychotherapist ',
     bio: ' registered nurse who is trained in the practice of psychiatric and mental health nursing. Trained to diagnose and provide individual and group counseling. ',
@@ -175,7 +175,7 @@ const Therapists = [
   },
   {
     id: 4,
-    cvLink: 's3://bucket-name/cv_33333333333333.pdf',
+    cvLink: 'https://drive.google.com/file/d/1jthZD5IlhwMn5IBUoQ-IPj3vnsuVsXf9/view?usp=sharing',
     profileImg: 'https://www.felixhospital.com/sites/default/files/2022-11/dr-dk-gupta.jpg',
     major: 'Marital and Family Therapist ',
     bio: " counselor with a master's degree, with special education and training in marital and family therapy. Trained to diagnose and provide individual and group counseling.  ",
@@ -184,7 +184,7 @@ const Therapists = [
   },
   {
     id: 5,
-    cvLink: 's3://bucket-name/cv_455555555555.pdf',
+    cvLink: 'https://drive.google.com/file/d/1jthZD5IlhwMn5IBUoQ-IPj3vnsuVsXf9/view?usp=sharing',
     profileImg: 'https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg',
     major: 'Pastoral Counselor  ',
     bio: ' clergy with training in clinical pastoral education. Trained to diagnose and provide individual and group counseling.   ',
@@ -193,7 +193,7 @@ const Therapists = [
   },
   {
     id: 6,
-    cvLink: 's3://bucket-name/cv_4666666666666.pdf',
+    cvLink: 'https://drive.google.com/file/d/1jthZD5IlhwMn5IBUoQ-IPj3vnsuVsXf9/view?usp=sharing',
     profileImg: 'https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg',
     major: 'Clinical Social Worker  ',
     bio: "  A counselor with a master's degree in social work from an accredited graduate program. Trained to make diagnoses A counselor with a master's degree in social work from an accredited graduate program. Trained to make diagnoses,   ",
@@ -202,7 +202,7 @@ const Therapists = [
   },
   {
     id: 7,
-    cvLink: 's3://bucket-name/cv_4555557777555.pdf',
+    cvLink: 'https://drive.google.com/file/d/1jthZD5IlhwMn5IBUoQ-IPj3vnsuVsXf9/view?usp=sharing',
     profileImg: 'https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg',
     major: 'Pastoral Counselor  ',
     bio: ' clergy with training in clinical pastoral education. Trained to diagnose and provide individual and group counseling.   ',
@@ -211,7 +211,7 @@ const Therapists = [
   },
   {
     id: 8,
-    cvLink: 's3://bucket-name/cv_452323227777555.pdf',
+    cvLink: 'https://drive.google.com/file/d/1jthZD5IlhwMn5IBUoQ-IPj3vnsuVsXf9/view?usp=sharing',
     profileImg: 'https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg',
     major: 'Psychiatrist  ',
     bio: ' A medical doctor with special training in the diagnosis and treatment of mental and emotional illnesses.  A psychiatrist can prescribe medication, but they often do not counsel patients.   ',
@@ -220,7 +220,7 @@ const Therapists = [
   },
   {
     id: 9,
-    cvLink: 's3://bucket-name/cv_45232323423423555.pdf',
+    cvLink: 'https://drive.google.com/file/d/1jthZD5IlhwMn5IBUoQ-IPj3vnsuVsXf9/view?usp=sharing',
     profileImg: 'https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg',
     major: 'Psychiatric or Mental Health Nurse Practitioner  ',
     bio: ' A registered nurse practitioner with a graduate degree and specialized training in the diagnosis and treatment of mental and emotional illness.    ',
@@ -229,7 +229,7 @@ const Therapists = [
   },
   {
     id: 10,
-    cvLink: 's3://bucket-name/cv_45232323423423222.pdf',
+    cvLink: 'https://drive.google.com/file/d/1jthZD5IlhwMn5IBUoQ-IPj3vnsuVsXf9/view?usp=sharing',
     profileImg: 'https://t4.ftcdn.net/jpg/02/60/04/09/360_F_260040900_oO6YW1sHTnKxby4GcjCvtypUCWjnQRg5.jpg',
     major: 'Clinical Psychologist  ',
     bio: ' A registered nurse practitioner with a graduate degree and specialized training in the diagnosis and treatment of mental and emotional illness.    ',
@@ -274,39 +274,144 @@ const Therapists = [
   },
 ];
 
-const appointments = [
+const Appointments = [
   {
     id: 1,
-    therapist_id: 1,
-    datetime: '6/2/2023 11:00',
+    therapistId: 1,
+    datetime: '6-2-2023 11:00',
     isBooked: 'false',
     isAvailable: 'true',
   },
   {
     id: 2,
-    therapist_id: 1,
-    datetime: '6/2/2023 12:00',
+    therapistId: 1,
+    datetime: '6-2-2023 12:00',
     isBooked: 'false',
     isAvailable: 'true',
   },
   {
     id: 3,
-    therapist_id: 1,
-    datetime: '6/2/2023 13:00',
+    therapistId: 1,
+    datetime: '6-2-2023 13:00',
     isBooked: 'false',
     isAvailable: 'true',
   },
   {
     id: 4,
-    therapist_id: 1,
-    datetime: '6/2/2023 14:00',
+    therapistId: 1,
+    datetime: '6-2-2023 14:00',
     isBooked: 'false',
     isAvailable: 'true',
   },
   {
     id: 5,
-    therapist_id: 1,
-    datetime: '6/2/2023 15:00',
+    therapistId: 1,
+    datetime: '6-2-2023 15:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 6,
+    therapistId: 2,
+    datetime: '6-2-2023 11:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 7,
+    therapistId: 2,
+    datetime: '6-2-2023 12:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 8,
+    therapistId: 2,
+    datetime: '6-2-2023 13:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 9,
+    therapistId: 2,
+    datetime: '6-2-2023 14:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 10,
+    therapistId: 2,
+    datetime: '6-2-2023 15:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 11,
+    therapistId: 3,
+    datetime: '6-2-2023 11:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 12,
+    therapistId: 3,
+    datetime: '6-2-2023 12:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 13,
+    therapistId: 3,
+    datetime: '6-2-2023 13:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 14,
+    therapistId: 3,
+    datetime: '6-2-2023 14:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 15,
+    therapistId: 3,
+    datetime: '6-2-2023 15:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 16,
+    therapistId: 4,
+    datetime: '6-2-2023 11:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 17,
+    therapistId: 4,
+    datetime: '6-2-2023 12:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 18,
+    therapistId: 4,
+    datetime: '6-2-2023 13:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 19,
+    therapistId: 4,
+    datetime: '6-2-2023 14:00',
+    isBooked: 'false',
+    isAvailable: 'true',
+  },
+  {
+    id: 20,
+    therapistId: 4,
+    datetime: '6-2-2023 15:00',
     isBooked: 'false',
     isAvailable: 'true',
   },
@@ -321,7 +426,7 @@ const Sessions = [
   {
     id: 2,
     appointment_id: 2,
-    userId: 2,
+    userId: 3,
   },
   {
     id: 3,
@@ -331,6 +436,16 @@ const Sessions = [
   {
     id: 4,
     appointment_id: 4,
+    userId: 3,
+  },
+  {
+    id: 5,
+    appointment_id: 5,
+    userId: 4,
+  },
+  {
+    id: 6,
+    appointment_id: 6,
     userId: 4,
   },
 
