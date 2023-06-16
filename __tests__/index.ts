88f2063f15dc025ed4b37ba0d4d3c2e117477633
MohannadSabaa <Moh4nn4d@gmail.com>
2023-06-16 23:16:12 +0300

    refactor: refactored appointment test

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,77 +1,12 @@
-import request from 'supertest';
-import app from '.././src/app';
+
 import connection from '../src/db/connection'
 import buildb from '../src/db/build'
 
-
+import therapistTest from './therapist';
+import appointmentRouteTest from './appointment';
 beforeAll(buildb);
 afterAll(() => connection.close())
 
- describe('GET appointment Router with therapist id and appointments date', () => {
-  it('Should return a Success Response with 200 status and result attributes',async () => {
-  
-    const response = await request(app).get(`/appointments/4?date=2023-06-18`);
-  
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual({
-        "data": [
-            {
-                "id": 16,
-                "datetime": "2023-06-18T08:00:00.000Z",
-                "therapistId": 4,
-                "isBooked": false,
-                "isAvailable": true
-            },
-            {
-                "id": 17,
-                "datetime": "2023-06-18T09:00:00.000Z",
-                "therapistId": 4,
-                "isBooked": false,
-                "isAvailable": true
-            },
-            {
-                "id": 18,
-                "datetime": "2023-06-18T10:00:00.000Z",
-                "therapistId": 4,
-                "isBooked": false,
-                "isAvailable": true
-            },
-            {
-                "id": 19,
-                "datetime": "2023-06-18T11:00:00.000Z",
-                "therapistId": 4,
-                "isBooked": false,
-                "isAvailable": true
-            },
-            {
-                "id": 20,
-                "datetime": "2023-06-18T12:00:00.000Z",
-                "therapistId": 4,
-                "isBooked": false,
-                "isAvailable": true
-            }
-        ],
-        "message": "appointment successful"
-    });
-  })
-
-  it('Should return a FAIL Response with 200 status and result attributes',async () => {
-    const response = await request(app).get(`/appointments/4`);
-  
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual({
-        "data": [],
-        "message": "sorry but no appointments found"
-  });
-  })
-});
-import buildDB from "../src/db/build";
-import dbConnection from "../src/db/connection";
-import therapistTest from './therapist';
-
-
-beforeAll(buildDB);
-afterAll(() => dbConnection.close());
-
 describe('Therapist Route', therapistTest);
+describe('Appointment Route', appointmentRouteTest)
 
--- __tests__/therapist.ts
+++ __tests__/therapist.ts
@@ -4,7 +4,7 @@ import app from '.././src/app';
 const therapistTest = () => {
     it('should return therapist information when valid ID is provided', async () => {
         const id = '3';
-        const response = await request(app).get(`/api/v1/therapists/${id}`);
+        const response = await request(app).get(`/therapists/${id}`);
         expect(response.status).toBe(200);
         expect(response.body.data.userId).toBe(6);
     });
@@ -12,13 +12,13 @@ const therapistTest = () => {
 
     it('should return 404 when invalid ID is provided', async () => {
         const id = '40';
-        const response = await request(app).get(`/api/v1/therapists/${id}`);
+        const response = await request(app).get(`/therapists/${id}`);
         expect(response.status).toBe(404);
     });
 
     it('check if therapist is not Active return 404 not found ',async () => {
         const id = '1';
-        const response = await request(app).get(`/api/v1/therapists/${id}`);
+        const response = await request(app).get(`/therapists/${id}`);
         expect(response.status).toBe(404);
     })
 }
