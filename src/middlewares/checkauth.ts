1167aa39b7f2cbef75d31983cb043a597d8b72f1
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-06-18 11:12:03 +0300

    build: add Authentication middleware function

---
+++ src/middlewares/checkauth.ts
@@ -0,0 +1,25 @@
+/* eslint-disable max-len */
+import { Response, NextFunction } from 'express';
+import { templateErrors } from '../helpers';
+import verifyToken from '../helpers/jwtverify';
+import { Decode, RequestWithUserRole, Roles } from '../types';
+
+const checkAuth = (role: Roles) => async (req: RequestWithUserRole, res: Response, next: NextFunction) => {
+  try {
+    const { token } = req;
+    if (token) {
+      const decoded = await verifyToken(token) as Decode;
+      req.user = decoded;
+      if (decoded.role !== role) {
+        throw templateErrors.UNAUTHORIZED('Unauthorized');
+      }
+      next();
+    } else {
+      throw templateErrors.UNAUTHORIZED('Unauthorized ');
+    }
+  } catch (error) {
+    next(templateErrors.UNAUTHORIZED('Unauthorized '));
+  }
+};
+
+export default checkAuth;
