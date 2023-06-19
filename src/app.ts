daef852e2e7a43cc560d74c63609728edb29c3f4
LinaYahya <linaebe0@gmail.com>
2023-06-19 21:32:37 +0300

    build: use morgan and cors

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ import dotenv from 'dotenv';
 
 import express from 'express';
 import bearerToken from 'express-bearer-token';
+import morgan from 'morgan';
+import cors from 'cors';
 import { clientError, serverError } from './middlewares';
 import router from './routes';
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(bearerToken());
+app.use(morgan('dev'));
 
 app.use('/api/v1', router);
 app.use(clientError);
