1f0bd1719714218e7e98e8742aaeb20ea96dc07e
MohannadSabaa <Moh4nn4d@gmail.com>
2023-06-19 12:25:11 +0300

    fix: conflict fixed confing file

--- src/services/nodemailer.ts
+++ src/services/nodemailer.ts
@@ -1,12 +1,11 @@
-import nodemailer from 'nodemailer';
+import nodemailer, { SendMailOptions } from 'nodemailer';
 import config from '../config';
 
 const { MAILER: { user, pass } } = config;
 
-const mailer = async (info: any) => {
+const mailer = async (info:SendMailOptions) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
-    port: 587,
     secure: false,
     auth: {
       user,
