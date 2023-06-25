e81eec3a9012519451e870a4bd480ac73930798f
Yasser Alaa <87650778+YasserAmro0@users.noreply.github.com>
2023-06-25 12:56:21 +0300

    fix: refactor handle type error  sessionPrice

--- src/controllers/payment.ts
+++ src/controllers/payment.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Response } from 'express';
 import { getClientSecret } from '../services';
 import { RequestWithUserRole } from '../types';
+import { templateErrors } from '../helpers';
 
 const findClientSecret = async (req: RequestWithUserRole, res: Response, next: NextFunction) => {
   try {
     const { SessionPrice } = req.body;
 
     if (typeof SessionPrice !== 'number') {
-      throw new Error('Invalid price');
+      throw templateErrors.BAD_REQUEST('Invalid price');
     }
     const userId = req.user?.userId;
     const paymentIntent = await getClientSecret(SessionPrice, userId);
