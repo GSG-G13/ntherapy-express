fcc170f0c782cc8cf890e750467e1895b5980244
raghdarizq <raghdarizq2@gmail.com>
2023-06-26 09:25:36 +0300

    fix: edit in services

--- src/services/userLogin.ts
+++ src/services/userLogin.ts
@@ -1,10 +1,22 @@
-import { User } from '../models';
+import { User, Therapist } from '../models';
+import { RolesForSelect } from '../types';
 
 const LoginByEmail = async (email: string) => {
   const user = await User.findOne({
     attributes: ['id', 'email', 'password', 'isActive', 'role'],
     where: { email },
   });
+
+  if (user && user.role === RolesForSelect.therapist) {
+    const therapist = await Therapist.findOne({
+      where: { userId: user.id },
+      attributes: ['id'],
+    });
+    if (therapist) {
+      console.log(therapist.id);
+    }
+  }
+
   return user;
 };
 
