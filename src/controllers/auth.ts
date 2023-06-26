f1190accbecfa428ca16ffd5566b3e2cd8a9009b
raghdarizq <77439319+raghdarizq@users.noreply.github.com>
2023-06-26 12:06:09 +0300

    Update src/controllers/auth.ts
    Co-authored-by: Lina Ebeid <linaebe0@gmail.com>

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -31,7 +31,7 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
       throw templateErrors.BAD_REQUEST('Your account is not activated yet. Please check your email for activation instructions.');
     }
 
-    const payload: {
+    const payload: IPayload
       userId: number,
       role: string,
       therapistId?: number
