674be62d6d6b6d2a8bcc992de9f61be2ddeae935
raghdarizq <raghdarizq2@gmail.com>
2023-06-29 17:19:21 +0300

    build: add routs for getAuth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
-import { login } from '../controllers';
+import { login, getAuth } from '../controllers';
+import { checkToken } from '../middlewares';
 
 const router = Router();
 router.post('/login', login);
+router.get('/login', checkToken(), getAuth);
 
 export default router;
