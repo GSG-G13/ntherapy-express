33ac583051bbf96d21794091b15b1fbce95cd5be
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-07-18 20:48:33 +0300

    fix: fix route fro edit

--- src/routes/bug.ts
+++ src/routes/bug.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { checkAuth } from '../middlewares';
 import { RolesForSelect } from '../types';
-import { getAllBugs, createNewBug } from '../controllers/bug';
+import { getAllBugs, createNewBug, editBug } from '../controllers/bug';
 
 const router = Router();
 
 router.get('/', checkAuth(RolesForSelect.admin), getAllBugs);
 router.post('/', createNewBug);
+router.patch('/', checkAuth(RolesForSelect.admin), editBug);
 
 export default router;
